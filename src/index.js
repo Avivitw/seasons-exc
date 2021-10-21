4b3aa3f1b42af8437083fc551e5e806cef59fe57
Avivit Weissman <wavivit@gmail.com>
2021-10-21 15:03:54 -0700

    Added the App file for logic of the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  state = { lat: null, errMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error: {this.state.errMessage}</div>;
+    }
+    if (!this.state.errMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
