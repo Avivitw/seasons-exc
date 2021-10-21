f7809929a47db2104e5845288037fc23b2dad30f
Avivit Weissman <wavivit@gmail.com>
2021-10-21 15:38:00 -0700

    refactor the app content to an helper function

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errMessage && !this.state.lat) {
       return <div>Error: {this.state.errMessage}</div>;
     }
@@ -21,6 +21,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept the location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
